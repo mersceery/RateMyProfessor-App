acdc08355c6a1f12e41345186d4afabdcb80eaea
duymn2002hn <duy2002.1@gmail.com>
2023-06-19 18:35:12 +0200

    edit profile all done (with editing profile picture from gallery, saved on firebase storage)

--- app/src/main/java/com/example/chatappv2/EditProfile.java
+++ app/src/main/java/com/example/chatappv2/EditProfile.java
@@ -3,7 +3,9 @@ package com.example.chatappv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -34,8 +39,15 @@ public class EditProfile extends AppCompatActivity {
     private Button logoutBtn;
     private Button saveChangesBtn;
 
+    private Button changeProfilePicBtn;
+
+    private ImageView profilePic;
+
+    private static final int PICK_IMAGE_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         logoutBtn = findViewById(R.id.btnLogout2);
@@ -47,40 +59,45 @@ public class EditProfile extends AppCompatActivity {
         editTextEmail = findViewById(R.id.EditEmailEditTxt);
         editTextName = findViewById(R.id.editNameEditTxt);
         saveChangesBtn = findViewById(R.id.saveChangesBtn);
+        changeProfilePicBtn = findViewById(R.id.changeProfilePicBtn);
+        profilePic = findViewById(R.id.profilePictureinEditProfile);
+        changeProfilePicBtn = findViewById(R.id.changeProfilePicBtn);
+
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser == null) {
-            // User not logged in, show pop-up message
-            Toast.makeText(EditProfile.this, "You need to be logged in", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(EditProfile.this, LoginActivity.class);
-            startActivity(intent);
-            // User logged in, proceed to EditProfile activity
-        }
+
 
         String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-        editTextEmail.setText(userEmail);
         DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+        checkLoggedInUser(currentUser);
         usersRef.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        String userKey = snapshot.getKey();
-                        String name = snapshot.child("name").getValue(String.class);
-                        editTextName.setText(name);
-                        final String finalUserKey = userKey;
-                        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                String newEmail = editTextEmail.getText().toString().trim();
-                                String newName = editTextName.getText().toString().trim();
-                                usersRef.child(finalUserKey).child("email").setValue(newEmail);
-                                usersRef.child(finalUserKey).child("name").setValue(newName);
-                                currentUser.updateEmail(newEmail);
-                                Toast.makeText(EditProfile.this, "Changes saved successfully", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                    DataSnapshot snapshot = dataSnapshot.getChildren().iterator().next();
+                    String userKey = snapshot.getKey();
+                    String name = snapshot.child("name").getValue(String.class);
+                    String profilePicUrl = snapshot.child("profile_pic").getValue(String.class);
+                    Picasso.get()
+                            .load(profilePicUrl)
+                            .placeholder(R.drawable.andre_tate)
+                            .into(profilePic);
+                    editTextName.setText(name);
+                    editTextEmail.setText(userEmail);
+                    saveChangesBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String newEmail = editTextEmail.getText().toString().trim();
+                            String newName = editTextName.getText().toString().trim();
+                            usersRef.child(userKey).child("email").setValue(newEmail);
+                            usersRef.child(userKey).child("name").setValue(newName);
+                            currentUser.updateEmail(newEmail);
+                            Toast.makeText(EditProfile.this, "Changes saved successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-                    }
                 }
             }
 
@@ -138,6 +155,116 @@ public class EditProfile extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), EditProfile.class);
                 startActivity(intent);
                 finish(); // close the loginActivity properly
+            }
+        });
+
+        changeProfilePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
+            }
+        });
+    }
+
+    private void checkLoggedInUser(FirebaseUser currentUser) {
+        if (currentUser == null) {
+            // User not logged in, show pop-up message
+            Toast.makeText(EditProfile.this, "You need to be logged in", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(EditProfile.this, LoginActivity.class);
+            startActivity(intent);
+            // User logged in, proceed to EditProfile activity
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            profilePic.setImageURI(data.getData());
+            uploadImageToFirebaseDatabase(imageUri);
+            uploadImageToFirebaseStorage(imageUri);
+        }
+    }
+    private void uploadImageToFirebaseStorage(Uri imageUri) {
+        // Create a reference to the Firebase Storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        // Get the current user's email
+        String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        // Generate a unique filename for the image
+        String filename = userEmail + "_" + System.currentTimeMillis();
+
+        // Create a reference to the image file in Firebase Storage
+        StorageReference imageRef = storageRef.child("profile_pics").child(filename);
+
+        // Upload the image to Firebase Storage
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    // Image uploaded successfully
+                    // Get the download URL of the uploaded image
+                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        // Save the download URL to Firebase Database
+                        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+                        usersRef.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                        String imageUserKey = snapshot.getKey();
+                                        usersRef.child(imageUserKey).child("profile_pic").setValue(uri.toString());
+                                        Toast.makeText(EditProfile.this, "Profile picture uploaded successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    // User not found
+                                    Toast.makeText(EditProfile.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                // Handle any errors
+                                Toast.makeText(EditProfile.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }).addOnFailureListener(e -> {
+                        // Failed to get the download URL
+                        Toast.makeText(EditProfile.this, "Failed to upload profile picture", Toast.LENGTH_SHORT).show();
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    // Failed to upload the image
+                    Toast.makeText(EditProfile.this, "Failed to upload profile picture", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void uploadImageToFirebaseDatabase(Uri imageUri) {
+        String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        usersRef.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        String imageUserKey = snapshot.getKey();
+                        usersRef.child(imageUserKey).child("profile_pic").setValue(imageUri.toString());
+                        Toast.makeText(EditProfile.this, "Successfully changed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // User not found
+                    Toast.makeText(EditProfile.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle any errors
+                Toast.makeText(EditProfile.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
             }
         });
 
