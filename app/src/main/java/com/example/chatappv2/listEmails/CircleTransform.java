7c3cd732db7e2a0565320ed29b2e361d3e8f4271
duymn2002hn <duy2002.1@gmail.com>
2023-06-29 23:27:09 +0200

    email list and picture border done

--- app/src/main/java/com/example/chatappv2/listEmails/CircleTransform.java
+++ app/src/main/java/com/example/chatappv2/listEmails/CircleTransform.java
@@ -1,18 +1,21 @@
 package com.example.chatappv2.listEmails;
 
-import com.squareup.picasso.Transformation;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Shader;
+
 import com.squareup.picasso.Transformation;
 
 public class CircleTransform implements Transformation {
+    private static final int BORDER_COLOR = 0xFF847D7F; // Brown color
+    private static final int BORDER_WIDTH = 15; // Border width in pixels
+
     @Override
     public Bitmap transform(Bitmap source) {
         int size = Math.min(source.getWidth(), source.getHeight());
-
         int x = (source.getWidth() - size) / 2;
         int y = (source.getHeight() - size) / 2;
 
@@ -23,15 +26,21 @@ public class CircleTransform implements Transformation {
 
         Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
 
-        // Apply circular shape
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
-        BitmapShader shader = new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        BitmapShader shader = new BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
         paint.setShader(shader);
         paint.setAntiAlias(true);
 
         float radius = size / 2f;
+        // Draw outer circle with black border
         canvas.drawCircle(radius, radius, radius, paint);
+        paint.setShader(null);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(BORDER_COLOR);
+        paint.setStrokeWidth(BORDER_WIDTH);
+        // Draw border
+        canvas.drawCircle(radius, radius, radius - BORDER_WIDTH / 2, paint);
 
         squaredBitmap.recycle();
         return bitmap;
